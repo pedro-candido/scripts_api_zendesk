d481859d4bf765c0450c777493f4c89250300f4d
pedrocandi1000 <pedroferreirac@hotmail.com>
2020-03-12 08:31:42 -0300

    ok

--- Zendesk_Support/get_users.py
+++ Zendesk_Support/get_users.py
@@ -2,16 +2,29 @@ import requests
 import json
 from pprint import pprint
 from requests.auth import HTTPBasicAuth
-from pandas import DataFrame
+import pandas as pd
+# import csv
 
-user = 'user'
-password = 'password'
+user = 'Email'
+password = 'Senha'
+base_url = 'https://{subdominio}.zendesk.com/api/v2/users.json?page='
 
-x = requests.get('https://{subdominium}.zendesk.com/api/v2/users.json', auth=(user, password))
-x = x.json()
+all_users = []
+filtered_users = []
 
-x = DataFrame(x)
+for i in range(0,250):
+    url = base_url+str(i)
+    response = requests.get(url, auth=(user, password)).json()
+    users = response.get('users')
+    all_users += users
+    print(url)
 
 
-x.to_csv('new_x.csv')
-x.to_json('new_x.json')
+for user in all_users:
+    if(user['role'] == 'end-user'):
+        filtered_users.append(user['email'])
+
+filtered_users = pd.DataFrame(filtered_users)
+
+with pd.ExcelWriter('email_request.xlsx') as writer:
+    filtered_users.to_excel(writer, sheet_name="email_request")
